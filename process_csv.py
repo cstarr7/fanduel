b4fe5051d477277f9331ec0c09c7ae8434729f1f
Charles Starr <charles.starr7@gmail.com>
2017-12-28 20:59:41 -0600

    Initial commit

---
+++ process_csv.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Charlie
+# @Date:   2017-12-28 20:54:09
+# @Last Modified by:   Charlie
+# @Last Modified time: 2017-12-28 20:56:53
+
+from lxml import html
+import requests
+import sqlite3
